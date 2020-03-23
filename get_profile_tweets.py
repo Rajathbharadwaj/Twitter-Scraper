33ea65324a27425fe16f2297f799b6ec25c08925
Rajath Bharadwaj <rajathrajath940@gmail.com>
2020-03-24 02:52:00 +0530

    Update get_profile_tweets.py

--- get_profile_tweets.py
+++ get_profile_tweets.py
@@ -3,6 +3,14 @@ import datetime as dt
 from twitterscraper.query import query_tweets_from_user
 import pandas as pd
 from summary import summary
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Handle and json')
+parser.add_argument('-han','--handle', type=str, help='twitter handle')
+parser.add_argument('-j','--json',type=str, help='json filename with .json')
+args = parser.parse_args()
+
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if hasattr(obj, '__json__'):
@@ -20,33 +28,37 @@ class JSONEncoder(json.JSONEncoder):
                     not hasattr(getattr(obj, member), '__call__')}
 
         return json.JSONEncoder.default(self, obj)
-    
+
+
 def get_profile_tweets(handle, filename):
     profile = query_tweets_from_user(handle, limit=10)
     print('Loading...')
     with open(filename, "w", encoding="utf-8") as output:
         json.dump(profile, output, cls=JSONEncoder)
-    profile_dataframe = pd.read_json('my.json', encoding='utf-8')
-    
-    profile_dataframe.to_csv(filename[3:]+".csv")
+    profile_dataframe = pd.read_json(filename, encoding='utf-8')
+
     lstt = []
     for i, val in enumerate(profile_dataframe['links']):
-        if str(val)=='[]':
+        if str(val) == '[]':
             lstt.append('[]')
-        elif str(val[0][0:17])=='https://youtu.be/':
+        elif str(val[0][0:17]) == 'https://youtu.be/':
             pass
         else:
             summ = summary(profile_dataframe['links'][i][0])
             lstt.append(summ)
         profile_dataframe['summary'] = pd.DataFrame(lstt)
-        
+
         is_summary = []
-        for i, idx in enumerate(my_df['summary']):
-            if len(str(idx))>3:
+        for i, idx in enumerate(profile_dataframe['summary']):
+            if len(str(idx)) > 3:
                 is_summary.append(1)
             else:
                 is_summary.append(0)
-                
-        profile_dataframe['is_summary']= pd.DataFrame(is_summary)
-    
-    print('Loaded')    
+
+        profile_dataframe['is_summary'] = pd.DataFrame(is_summary)
+
+    profile_dataframe.to_csv(filename[:-5] + ".csv")
+    print('Loaded')
+
+if __name__ == '__main__':
+    get_profile_tweets(args.handle, args.json)
